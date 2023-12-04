763210f37dcdb22a157dde53b84e364f4be6b558
leewei <2955538482@qq.com>
2023-12-04 17:33:43 +0800

    feat: change into a new mode

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,20 @@
 <script setup lang="ts">
-import SideBar from "./components/SideBar.vue";
-import LoginIn from "./components/LoginIn.vue";
-import CollectPage from "./components/collectPage.vue";
-import { siderTagStore } from "./store/sideBarTag";
 import { storeToRefs } from "pinia";
-import editor from "./components/editor/editor.vue";
-import EditorTask from "./components/EditorTask.vue";
-import NotePage from "./components/NotePage.vue";
-import Loading from "./components/common-components/loading/loading.vue";
-import NoticePage from "./components/NoticePage.vue";
-import LoginedPage from "./components/loginedPage.vue";
-import Amessage from "src/components/common-components/Message";
 import { ref } from "vue";
 
-const store = siderTagStore();
 
-const { siderTag, getSiderTag } = storeToRefs(store);
-const { changeSiderTag } = store;
-
-const loginKeyPersistence = ref("");
 </script>
 
 <template>
   <div class="aminion-container">
-    <SideBar />
-    <LoginIn
-      v-if="siderTag == 'loginIn'"
-      :on-close="
-        () => {
-          changeSiderTag('');
-        }
-      "
-    />
-    <CollectPage :hidden="siderTag != 'collect'" />
-    <editor v-if="siderTag == 'edit-note'" />
-    <EditorTask v-if="siderTag == 'edit-task'" />
-    <NotePage :hidden="siderTag != 'note'" />
-    <NoticePage :hidden="siderTag != 'notice'" />
-    <LoginedPage :hidden="siderTag != 'login-menu'" />
-
-    <!-- <Loading type="line" fill="purple" :size="20"  /> -->
+    <p>登录页</p>
   </div>
 </template>
 
 <style scoped>
-.aminion-container {
-  position: fixed;
-  right: 50px;
-  top: 50%;
-  display: flex;
-  flex-direction: row-reverse;
-  transform: translate(0, -50%);
-  z-index: 9999;
-}
+
+  .aminion-container {
+    width: 300px;
+    height: 300px;
+  }
 </style>
